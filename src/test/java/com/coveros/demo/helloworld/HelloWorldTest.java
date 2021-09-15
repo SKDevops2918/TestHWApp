17f6be521cecb04c3615c371ab97d83f33536081
Suyog <suyog.kulkarni1987@hotmail.com>
2021-09-15 08:35:14 +0530

    first commit

---
+++ src/test/java/com/coveros/demo/helloworld/HelloWorldTest.java
@@ -0,0 +1,18 @@
+package com.coveros.demo.helloworld;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class HelloWorldTest {
+   @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
+
+
